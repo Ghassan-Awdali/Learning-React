7a95112e8c166baae2bb2a837e7debaa7c56611b
G ARUTO <awdalig@gmail.com>
2024-07-17 18:36:55 -0300

    passing in objects with props

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 
 const Fruits = () => {
-  const fruits = ["Apple", "Mango", "Banananannanana", "Chocolate"];
+  //const fruits = ["Apple", "Mango", "Banananannanana", "Chocolate"];
+
+  const fruits = [
+    { name: "Apple", price: 10, emoji: "apple" },
+    { name: "Mango", price: 13, emoji: "mango" },
+    { name: "Bananana", price: 2, emoji: "bananana" },
+  ];
   return (
     <div>
       <ul>
         {fruits.map((fruit) => (
-          <li key={fruit}> {fruit}</li>
+          <li key={fruit}>
+            {" "}
+            {fruit.name} {fruit.price} {fruit.emoji}
+          </li>
         ))}
       </ul>
     </div>
